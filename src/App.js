74ec5fceee423032f39efcfecfa7742321f31853
Moscow <afker2020@gmail.com>
2022-11-28 17:54:06 +0100

    phone finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
 import clipboardLogo from "./images/logo.svg";
 import computer from "./images/image-computer.png";
+import ipad from "./images/image-devices.png";
+import paper from "./images/icon-blacklist.svg";
+import text from "./images/icon-text.svg";
+import peak from "./images/icon-preview.svg";
+import google from "./images/logo-google.png";
+import ibm from "./images/logo-ibm.png";
+import microsoft from "./images/logo-microsoft.png";
+import entreprise from "./images/logo-hp.png";
+import vector from "./images/logo-vector-graphics.png";
+import facebook from "./images/icon-facebook.svg";
+import instagram from "./images/icon-instagram.svg";
+import twitter from "./images/icon-twitter.svg";
 
 function App() {
   return (
@@ -77,8 +89,117 @@ function App() {
           Whether you’re on the go, or at your computer, you can access all your
           Clipboard snippets in a few simple clicks.
         </p>
+        <img src={ipad} className=" w-[70%] mt-10  " alt="" />
       </section>
       {/*  */}
+      {/* 4th sec */}
+      <section className="mt-[10rem] flex flex-col items-center">
+        <h2 className=" font-[600] text-3xl text-center w-[60%] text-DarkGrayishBlue ">
+          Supercharge your workflow
+        </h2>
+        <p className="text-center text-GrayishBlue w-[65%] mt-4">
+          We’ve got the tools to boost your productivity.
+        </p>
+        <div>
+          <div className=" mt-20 flex flex-col items-center gap-2">
+            <img src={paper} className="mb-6" alt="" />
+            <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+              Create blacklists
+            </h3>
+            <p className=" text-GrayishBlue text-center w-[65%] ">
+              Ensure sensitive information never makes its way to your clipboard
+              by excluding certain sources.
+            </p>
+          </div>
+          <div className=" mt-20 flex flex-col items-center gap-2">
+            <img src={text} className="mb-6" alt="" />
+            <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+              Plain text snippets
+            </h3>
+            <p className=" text-GrayishBlue text-center w-[65%] ">
+              Remove unwanted formatting from copied text for a consistent look.
+            </p>
+          </div>
+          <div className=" mt-20 flex flex-col items-center gap-2">
+            <img src={peak} className="mb-6" alt="" />
+            <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+              Sneak preview
+            </h3>
+            <p className=" text-GrayishBlue text-center w-[65%] ">
+              Quick preview of all snippets on your Clipboard for easy access.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/*  */}
+      {/* 5th sec */}
+      <section className="mt-[10rem] flex gap-[4rem] flex-col items-center">
+        <img src={google} alt="" />
+        <img src={ibm} alt="" />
+        <img src={microsoft} alt="" />
+        <img src={entreprise} alt="" />
+        <img src={vector} alt="" />
+      </section>
+      {/*  */}
+      {/* 6th sec */}
+      <section className="mt-[10rem] pb-20 flex  flex-col items-center">
+        <h2 className=" font-[600] text-3xl text-center w-[60%] text-DarkGrayishBlue ">
+          Clipboard for iOS and Mac OS
+        </h2>
+        <p className="text-center text-GrayishBlue w-[60%] mt-4">
+          Available for free on the App Store. Download for Mac or iOS, sync
+          with iCloud and you’re ready to start adding to your clipboard.
+        </p>
+        <div className="flex flex-col gap-6 mt-8 w-[100%] items-center ">
+          <button className=" bg-StrongCyan rounded-full w-[70%] py-4 shadow-xl text-white">
+            Download for iOS
+          </button>
+          <button className=" bg-LightBlue rounded-full w-[70%] py-4 shadow-xl text-white">
+            Download for Mac
+          </button>
+        </div>
+      </section>
+      {/*  */}
+      <footer className=" bg-Grayish mt-[10rem] pb-20 flex  flex-col items-center">
+        <img src={clipboardLogo} className=" w-[20%] mt-8 " alt="" />
+        <ul className=" flex flex-col items-center gap-4 mt-8">
+          <li>
+            {" "}
+            <a className=" font-[600] text-DarkGrayishBlue " href="#">
+              FAQs
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a className=" font-[600] text-DarkGrayishBlue" href="#">
+              Contact Us
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a className=" font-[600] text-DarkGrayishBlue" href="#">
+              Privacy Policy
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a className=" font-[600] text-DarkGrayishBlue" href="#">
+              Press Kit
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a className=" font-[600] text-DarkGrayishBlue" href="#">
+              Install Guide
+            </a>{" "}
+          </li>
+        </ul>
+        <div className=" mt-10 w-[100%] gap-10 flex justify-center items-center">
+          <img className=" cursor-pointer" src={facebook} alt="" />
+          <img className=" cursor-pointer" src={twitter} alt="" />
+          <img className=" cursor-pointer  " src={instagram} alt="" />
+        </div>
+      </footer>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
 
         DarkGrayishBlue: "hsl(210, 10%, 33%)",
         GrayishBlue: "hsl(201, 11%, 66%)",
+        Grayish: "hsl(201, 11%, 93%)",
       },
       fontFamily: {
         Bai: ["Bai Jamjuree"],
