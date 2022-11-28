7f6b37b62f4af6bcad8d8d83076e634de3c4062d
Moscow <afker2020@gmail.com>
2022-11-28 17:15:25 +0100

    2nd section over

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import computer from "./images/image-computer.png";
 function App() {
   return (
     <main className=" font-Bai">
+      {/* 1st sec */}
       <section className="  pt-20 gap-4   flex flex-col items-center bg-[url('./images/bg-header-mobile.png')] bg-no-repeat bg-right-top bg-cover  ">
         <img className="mt-8" src={clipboardLogo} alt="" />
         <h1 className=" mt-10 font-[600] text-3xl text-center w-[65%] text-DarkGrayishBlue  ">
@@ -23,6 +24,8 @@ function App() {
           </button>
         </div>
       </section>
+      {/* ********** */}
+      {/* 2nd sec */}
       <section className=" mt-[10rem] flex flex-col items-center ">
         <h2 className=" font-[600] text-3xl text-center w-[60%] text-DarkGrayishBlue ">
           Keep track of your snippets
@@ -34,22 +37,48 @@ function App() {
         </p>
         <div className="mt-20 flex flex-col items-center ">
           <img src={computer} className=" w-[80%] " alt="" />
-          <div>
-            <h3>Quick Search</h3>
-            <p>
-              Easily search your snippets by content, category, web address,
-              application, and more.
-            </p>
-            <h3>iCloud Sync</h3>
-            <p>Instantly saves and syncs snippets across all your devices.</p>
-            <h3>Complete History</h3>
-            <p>
-              Retrieve any snippets from the first moment you started using the
-              app.
-            </p>
+          <div className=" flex flex-col items-center gap-8 mt-10  ">
+            <div className=" flex flex-col items-center gap-2">
+              <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+                Quick Search
+              </h3>{" "}
+              <p className=" text-GrayishBlue text-center w-[65%] ">
+                Easily search your snippets by content, category, web address,
+                application, and more.
+              </p>
+            </div>
+            <div className=" flex flex-col items-center gap-2">
+              <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+                iCloud Sync
+              </h3>
+              <p className=" text-GrayishBlue text-center w-[60%] ">
+                Instantly saves and syncs snippets across all your devices.
+              </p>
+            </div>
+            <div className=" flex flex-col items-center gap-2">
+              <h3 className=" font-[600] text-xl  text-DarkGrayishBlue ">
+                Complete History
+              </h3>
+              <p className=" text-GrayishBlue text-center w-[65%] ">
+                Retrieve any snippets from the first moment you started using
+                the app.
+              </p>
+            </div>
           </div>
         </div>
       </section>
+      {/* *********** */}
+      {/* 3rd sec */}
+      <section className="mt-[10rem] flex flex-col items-center">
+        <h2 className=" font-[600] text-3xl text-center w-[60%] text-DarkGrayishBlue ">
+          Access Clipboard anywhere
+        </h2>
+        <p className="text-center text-GrayishBlue w-[65%] mt-4">
+          Whether you’re on the go, or at your computer, you can access all your
+          Clipboard snippets in a few simple clicks.
+        </p>
+      </section>
+      {/*  */}
     </main>
   );
 }
